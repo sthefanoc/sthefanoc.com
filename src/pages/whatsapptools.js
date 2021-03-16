a6f3d5f5e0e821621b0fc1ae79a4434dc2e2a449
Sthefano Carvalho <sthefano@masteradin.com>
2021-03-15 23:08:17 -0300

    config: added redirect to project page

---
+++ src/pages/whatsapptools.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function whatsapptools() {
+  window.location.href =
+    "https://chrome.google.com/webstore/detail/whatsapp-tools/akbdcpijgcaccjnjmcedafaojjgodcfe"
+  return <div>Redirecting to Chrome Store...</div>
+}
+
+export default whatsapptools
